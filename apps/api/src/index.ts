e5a2032497f2bfdb599c2d7c27a4748014036f76
Paxton Terry <paxtonterrydev@gmail.com>
2026-01-31 18:23:25 -0700

    refactor: removes old log statement

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,20 +1,17 @@
-import { Hono } from 'hono'
-import { etag } from 'hono/etag'
-import { logger } from 'hono/logger'
-import { Server } from './server.js'
-import { config } from './lib/config/config.js'
+import { Hono } from "hono";
+import { etag } from "hono/etag";
+import { logger } from "hono/logger";
+import { Server } from "./server.js";
+import { config } from "./lib/config/config.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use(
-  etag(),
-  logger()
-)
+app.use(etag(), logger());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-const server = new Server(app)
-console.dir(config, { depth: null}) 
+const server = new Server(app);
+
 server.start(config.port);
