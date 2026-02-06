401dbe507bbc88c9ec8659bc85eeda9667862169
Paxton Terry <paxtonterrydev@gmail.com>
2026-02-06 14:14:16 -0700

    feat: adds method to get application's users

--- apps/api/src/lib/repositories/application.ts
+++ apps/api/src/lib/repositories/application.ts
@@ -1,6 +1,22 @@
 import { PrismaClient } from "../../../generated/prisma/client.js";
 import { Repository } from "./repository.js";
 
+// TODO: Move to shared types file
+interface PaginationRequest {
+  skip: number;
+  take: number;
+}
 export class ApplicationRepository extends Repository {
-
+  // TODO: we are just using default offset pagination for now -> eventually, we will want to move to cursor based pagination once we introduce caching
+  async getApplicationUsers(id: string, pagination?: PaginationRequest): Promise<User[]> {
+    return this.db.application.findMany({
+      { pagination && }
+  include: {
+    users: true,
+  },
+  where: {
+    id
+  }
+})
+  }
 }
