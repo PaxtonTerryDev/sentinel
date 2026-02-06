935a1f1bdc5dcbc25d31cddbe03c8dba4df0713d
Paxton Terry <paxtonterrydev@gmail.com>
2026-02-06 15:07:37 -0700

    chore: updates api package dependencies and tsconfig
    chore: updates api database type import paths
    
    chore: install and build

--- apps/api/src/lib/repositories/application.ts
+++ apps/api/src/lib/repositories/application.ts
@@ -1,4 +1,4 @@
-import type { User } from "@/generated/prisma/browser.js";
+import type { User } from "@workspace/database"
 import { Repository } from "./repository.js";
 
 // TODO: Move to shared types file
@@ -6,17 +6,18 @@ interface PaginationRequest {
   skip: number;
   take: number;
 }
+
+// TODO: we are just using default offset pagination for now -> eventually, we will want to move to cursor based pagination once we introduce caching
 export class ApplicationRepository extends Repository {
-  // TODO: we are just using default offset pagination for now -> eventually, we will want to move to cursor based pagination once we introduce caching
   async getApplicationUsers(id: string, pagination?: PaginationRequest): Promise<User[]> {
     return this.db.application.findMany({
-      { pagination && }
-  include: {
-    users: true,
-  },
-  where: {
-    id
-  }
-})
+      ...pagination, // only includes if provided
+      include: {
+        users: true,
+      },
+      where: {
+        id
+      }
+    })
   }
 }
